2f04149112cb2217bfa77c659e027872974c1f3a
saveffer1 <saveffer01@gmail.com>
2023-08-29 12:32:20 +0700

    refactoring main code and changed to state by state

--- code/main.py
+++ code/main.py
@@ -1,23 +1,39 @@
 import pygame
-from pygame.locals import *
 import sys
 from settings import *
-from menu import*
-        
+from gamestate import *
+
 class Game:
-    def __init__(self) -> None:
+    def __init__(self):
         pygame.init()
-        self.display_info = pygame.display.Info()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE, pygame.FULLSCREEN if config.getint('GRAPHIC', 'SCREEN_CODE') == 0 else 0)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN if config.getint('GRAPHIC', 'SCREEN_CODE') == 0 else 0)
         self.title = pygame.display.set_caption('Idle Rich')
         self.icon = pygame.image.load('assets/tired.png')
         pygame.display.set_icon(self.icon)
-        self.clock = pygame.time.Clock()
-        self.main_menu = MainMenu()
+
+        self.states = {
+            'main_menu': MainMenu(self),
+            'pause_menu': PauseMenu(self),
+            'option_menu': OptionMenu(self),
+            'game_play': GamePlay(self)
+        }
+        self.current_state = 'main_menu'
+        self.prev_state = None
+
+    def change_state(self, new_state):
+        if new_state in self.states:
+            self.prev_state = self.current_state
+            self.current_state = new_state
             
     def run(self):
-        self.main_menu.open()
-
+        while True:
+            events = pygame.event.get()
+            
+            # Handle events and update/render the current state
+            self.states[self.current_state].handle_events(events)
+            self.states[self.current_state].update()
+            self.states[self.current_state].render()
+            
 if __name__ == '__main__':
     game = Game()
     game.run()
